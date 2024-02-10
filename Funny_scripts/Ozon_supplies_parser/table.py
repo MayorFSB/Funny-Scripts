82ae6b081d057877fdd86676758b37e25f9090c0
MayorFSB <@>
2024-02-10 12:45:42 +0300

    Переписал через классы

--- Funny_scripts/Ozon_supplies_parser/table.py
+++ Funny_scripts/Ozon_supplies_parser/table.py
@@ -1,52 +1,101 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
 import sqlite3
-file_db = 'ozon_prices.db'
-# Создаем приложение
-app = QApplication([])
-window = QMainWindow()
-# Создаем таблицу
-table = QTableWidget(window)
-connection = sqlite3.connect(file_db)
-data = connection.cursor()
-name_db = 'ozon'
-links = data.execute(f'SELECT link FROM {name_db}').fetchall()
-cols = data.execute(f'PRAGMA table_info({name_db})').fetchall()
-table.setRowCount(len(links))  # Устанавливаем количество строк
-table.setColumnCount(len(cols))  # Устанавливаем количество столбцов
-col_d = [cols[x][1] for x in range(1, len(cols))]
-table.setHorizontalHeaderLabels(col_d)   # Называем столбцы своими именами
-for i in range(1, len(links)+1):
-    h = data.execute(f'SELECT * FROM {name_db} WHERE id = ?', (i, )).fetchall()[0]
-    for j in range(len(h)):
-        table.setItem(i-1, j-1, QTableWidgetItem(h[j]))
-
-con = sqlite3.connect(file_db)
-cursor = con.cursor()
-d = ', '.join([f'{cols[xx][1]} TEXT' for xx in range(1, len(cols))])
-cursor.execute(f"CREATE TABLE IF NOT EXISTS ozon (id INTEGER PRIMARY KEY, {d})")
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QWidget, QVBoxLayout, QLabel, \
+    QLineEdit, QPushButton
 
 
-def update_cols():
-    for row in range(table.rowCount()):
-        for id,column in enumerate(col_d):
-            new_value = table.item(row, id).text()
-            k = cursor.execute(f'SELECT {column} FROM {name_db} WHERE id = ?', (row+1, )).fetchall()[0][0]
-            if new_value == k:
-                pass
-            else:
-                #item = QTableWidgetItem(new_value)
-                print(f'В колонке {column} и строке {row} новое значение: {new_value}')
-                cursor.execute(f"UPDATE ozon SET {column} = ? WHERE id = ?", (new_value, row+1))
-                con.commit()
+class DatabaseApp(QMainWindow):
+    def __init__(self, file_db_):
+        super().__init__()
 
-# Добавляем таблицу в окно
-window.setCentralWidget(table)
-# Задаем размер окна
-window.resize(800, 600)
-# Отображаем окно
-window.show()
+        self.file_db = file_db_
 
-# Запускаем главный цикл приложения
-app.exec_()
-update_cols()
-con.close()
+        # Создаем таблицу
+        self.table = QTableWidget()
+        self.setCentralWidget(self.table)
+
+        # Подключаемся к базе
+        self.connection = sqlite3.connect(self.file_db)
+        self.data = self.connection.cursor()
+        self.name_db = 'ozon'
+        self.links = self.data.execute(f'SELECT link FROM {self.name_db}').fetchall()
+        self.cols = self.data.execute(f'PRAGMA table_info({self.name_db})').fetchall()
+
+        # Получаем количество строк и столбцов с именами в таблице для отрисовки
+        self.table.setRowCount(len(self.links))
+        self.table.setColumnCount(len(self.cols))
+        self.col_names = [self.cols[x][1] for x in range(1, len(self.cols))]
+        self.table.setHorizontalHeaderLabels(self.col_names)
+
+        # Построчно отрисовываем таблицу получая данные из базы
+        for i in range(1, len(self.links) + 1):
+            db_string = self.data.execute(f'SELECT * FROM {self.name_db} WHERE id = ?', (i,)).fetchall()[0]
+            for j in range(len(db_string)):
+                self.table.setItem(i - 1, j - 1, QTableWidgetItem(db_string[j]))
+
+        # Создаем описание кнопок и окна ввода
+        self.input_label = QLabel("Новое значение:")
+        self.input_text = QLineEdit()
+        self.save_button = QPushButton("Сохранить ссылку на новый товар")
+        self.update_button = QPushButton("Обновить цены за сегодня")
+        self.delete_button = QPushButton("Удалить выбранную строку")
+
+        # Создаем виджет для поля ввода и кнопок
+        self.input_widget = QWidget()
+        self.input_layout = QVBoxLayout()
+        self.input_layout.addWidget(self.input_label)
+        self.input_layout.addWidget(self.input_text)
+        self.input_layout.addWidget(self.save_button)
+        self.input_layout.addWidget(self.update_button)
+        self.input_layout.addWidget(self.delete_button)
+        self.input_widget.setLayout(self.input_layout)
+
+        # Создаем главный виждет и добавляем в него  кнопки и таблицу
+        self.main_widget = QWidget()
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.input_widget)
+        self.main_layout.addWidget(self.table)
+        self.main_widget.setLayout(self.main_layout)
+
+        # Отрисовываем виджет с кнопками по центру
+        self.setCentralWidget(self.main_widget)
+
+        # Подключаем нажатия кнопок к функциям
+        self.save_button.clicked.connect(self.save_value)   # noqa Cannot find reference 'connect' in 'pyqtSignal \
+        self.update_button.clicked.connect(self.update_value)   # noqa | pyqtSignal | function'
+        self.delete_button.clicked.connect(self.delete_value)   # noqa не знаю как исправить
+
+    def save_value(self):
+        pass
+
+    def update_cols(self):
+        for row in range(self.table.rowCount()):
+            for i, column in enumerate(self.col_names):
+                new_value = self.table.item(row, i).text()
+                db_value = self.data.execute(
+                    f'SELECT {column} FROM {self.name_db} WHERE id = ?', (row + 1,)
+                ).fetchall()[0][0]
+                if new_value == db_value:
+                    pass
+                else:
+                    print(f'В колонке {column} и строке {row} новое значение: {new_value}')
+                    self.data.execute(f"UPDATE {self.name_db} SET {column} = ? WHERE id = ?", (new_value, row + 1))
+                    self.connection.commit()
+
+    def update_value(self):
+        pass
+
+    def delete_value(self):
+        pass
+
+    def closeEvent(self, event):
+        self.update_cols()
+        event.accept()
+
+
+if __name__ == '__main__':
+    file_db = 'ozon_prices.db'
+    app = QApplication([])
+    window = DatabaseApp(file_db)
+    window.resize(800, 600)
+    window.show()
+    app.exec_()
